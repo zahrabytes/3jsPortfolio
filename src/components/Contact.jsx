4bc193d6dac5be0c1caddd3a5470cf12e3808bda
Zahra Bukhari <bukhaz99@gmail.com>
2024-08-01 12:55:01 -0500

    typewriter and stars

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,6 +27,57 @@ const Contact = () => {
       className='flex-[0.75] bg-[#001418] p-8 rounded-2xl'>
         <p className={`${styles.sectionSubText} text-[#FFEBCD]`}>Get in Touch</p>
         <h3 className={`${styles.sectionHeadText} text-[#FFEBCD]`}>Contact.</h3>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+          >
+            <label className='flex flex-col'>
+              <span className='text-[#FFEBCD] font-medium mb-4'>Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                placeholder="What's your name?"
+                className='bg-black py-4 px-6 placeholder:text-[#b3a58f] text-[#FFEBCD] rounded-lg outline-none border-none font-medium'
+                />
+            </label>
+            <label className='flex flex-col'>
+              <span className='text-[#FFEBCD] font-medium mb-4'>Email</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                placeholder="What's your email?"
+                className='bg-black py-4 px-6 placeholder:text-[#b3a58f] text-[#FFEBCD] rounded-lg outline-none border-none font-medium'
+                />
+            </label>
+            <label className='flex flex-col'>
+              <span className='text-[#FFEBCD] font-medium mb-4'>Message</span>
+              <textarea
+                rows="7"
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                placeholder="What do you want to say?"
+                className='bg-black py-4 px-6 placeholder:text-[#b3a58f] text-[#FFEBCD] rounded-lg outline-none border-none font-medium'
+                />
+            </label>
+            <button 
+              type="submit"
+              className='bg-black py-3 px-8 outline-non w-fit text-[#FFEBCD] font-bold shadow-md shadow-primary rounded-xl'
+              >
+              {loading ? 'Sending...' : 'Send'}
+            </button>
+        </form>
+      </motion.div>
+      <motion.div
+      variants={slideIn('right', "tween", 0.2, 1)}
+      className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas/>
       </motion.div>
     </div>
   )
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from 'react';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import CanvasLoader from "../Loader";
 
 const Earth = () => {
+  const earth = useGLTF('./typewriter/scene.gltf')
+
   return (
-    <div>Earth</div>
+    <mesh>
+      <hemisphereLight intensity={20}
+      groundColor="black" />
+      <pointLight intensity={20}/>
+      {/* Front light */}
+      <pointLight position={[0, 5, 10]} intensity={20} />
+      
+      {/* Left side light */}
+      <pointLight position={[-10, 5, 5]} intensity={20} />
+      
+      {/* Right side light */}
+      <pointLight position={[10, 5, 5]} intensity={20} />
+      
+      {/* Top light */}
+      <pointLight position={[0, 10, 0]} intensity={20} />
+      <spotLight
+      position={[-20,50,10]} 
+      angle={0.12}
+      penumbra={1}
+      intensity={10}/>
+    <primitive
+    object={earth.scene}
+    scale={8}
+    position-y={0}
+    rotation-x={1}  // Tilt downwards
+    rotation-y={-0.2} // Rotate slightly to the left
+    rotation-z={0.1}  // Optional: adds a slight diagonal tilt
+    />
+    </mesh>
   )
 }
 
-export default Earth
+const EarthCanvas = () => {
+  return (
+    <Canvas
+    shadows
+    frameloop='demand'
+    gl={{preserveDrawingBuffer: true}}
+    camera={{
+      fov:45, 
+      near: 0.1,
+      far: 200,
+      position: [-2,-7,6]}}>
+      <Suspense fallback={<CanvasLoader/>}>
+        <OrbitControls 
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}/>
+        <Earth/>
+      </Suspense>
+
+    </Canvas>
+  )
+}
+
+export default EarthCanvas;
