6f0454a4757072b4357da08741fe251bf32e32e4
Zahra Bukhari <bukhaz99@gmail.com>
2024-08-01 11:10:03 -0500

    removed a bunch of files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           >          
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
           <p className='text-[#008000] text-[18px] font-bold cursor-pointer flex'>
-            Zahra &nbsp;
+            Zahra&nbsp;
             <span className='sm:block hidden'>Bukhari | Portfolio</span>
             </p>
         </Link>
