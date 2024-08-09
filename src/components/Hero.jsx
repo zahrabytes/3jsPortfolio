0ce23a58b92858e2db099f6e58f9421989118290
Zahra Bukhari <bukhaz99@gmail.com>
2024-08-09 12:02:38 -0500

    remove

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,6 @@ const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div
-          className='w-5 h-5 rounded-full border-4 border-[#008000]'/>
-          <div
-           className='w-1 sm:h-80 h-40 green-gradient '/>
-        </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-[#FFEBCD] typing`}>Hello, I'm <span className='text-[#008000]' >Zahra</span>!</h1>
